2631473d08713d5c720807ee3213a30829f58432
Zachary Sirna <zsirna@yahoo.com>
2022-03-10 15:39:35 -0500

    commit tree 45d6891ec11432ee8e0bb88df50d983110cbf559

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 console.log('%c HI', 'color: firebrick')
+
+document.addEventListener('DOMContentLoaded', () => {
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then( resp => resp.json())
+  .then(data => console.log(data))
+})
